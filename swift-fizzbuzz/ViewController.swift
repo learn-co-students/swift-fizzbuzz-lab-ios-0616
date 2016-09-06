5db3da9341379d57b5031ecb9001ad8dcac9e761
Johann Kerr <johannkerr@gmail.com>
2016-09-06 13:50:22 -0400

    Initial Commit

---
+++ swift-fizzbuzz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-fizzbuzz
+//
+//  Created by Johann Kerr on 9/6/16.
+//  Copyright © 2016 Johann Kerr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
