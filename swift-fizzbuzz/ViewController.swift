f6858167c629979e2eb6eb048a32a5facd3bce74
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-09-29 09:41:45 -0400

    Done

--- swift-fizzbuzz/ViewController.swift
+++ swift-fizzbuzz/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        fizzBuzzExercise([3, 5, 6, 9, 10, 20, 25, 30, 36, 45, 50, ])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func fizzBuzzExercise(beginningArray: [Int]) -> Bool {
+        let fizz = "Fizz"
+        let buzz = "Buzz"
+        let fizzBuzz = "fizzBuzz"
+        var resultArray = [Any] ()
+        
+        for value in beginningArray {
+            if value % 3 == 0 {
+                resultArray.append(fizz)
+                print("\(value) is divisible by 3: \(resultArray)")
+            } else if value % 5 == 0 {
+                resultArray.append(buzz)
+                print("\(value) is divisible by 5: \(resultArray)")
+            } else if (value % 3) == 0 && (value % 5) == 0 {
+                resultArray.append(fizzBuzz)
+                print("\(value) is divisible by 3 & 5 \(resultArray)")
+            } else {
+                resultArray.append(value)
+                print("\(value) is not divisible by 3 or 5: \(resultArray)")
+            }
+        }
+        return true
     }
-
-
 }
+
 
