26915389c1f33387cf79960436b51e9b2708c1db
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-09-30 11:25:28 -0400

    Corrections

--- swift-fizzbuzz/ViewController.swift
+++ swift-fizzbuzz/ViewController.swift
@@ -12,31 +12,35 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        fizzBuzzExercise([3, 5, 6, 9, 10, 20, 25, 30, 36, 45, 50, ])
+        fizzBuzzExercise([15, 5, 6, 9, 10, 20, 25, 30, 36, 45, 50])
+//        fizzBuzzExercise([30,1,2,3,44,56,78,100,3,7,6,5,45])
     }
     
-    func fizzBuzzExercise(beginningArray: [Int]) -> Bool {
+    func fizzBuzzExercise(beginningArray: [Int]) {
         let fizz = "Fizz"
         let buzz = "Buzz"
         let fizzBuzz = "fizzBuzz"
         var resultArray = [Any] ()
         
         for value in beginningArray {
-            if value % 3 == 0 {
-                resultArray.append(fizz)
-                print("\(value) is divisible by 3: \(resultArray)")
+            if (value % 3) == 0 && (value % 5) == 0 {
+                resultArray.append(fizzBuzz)
+                print(resultArray)
+                print("\(value) is divisible by 3 & 5 \(resultArray)")
             } else if value % 5 == 0 {
                 resultArray.append(buzz)
+                print(resultArray)
                 print("\(value) is divisible by 5: \(resultArray)")
-            } else if (value % 3) == 0 && (value % 5) == 0 {
-                resultArray.append(fizzBuzz)
-                print("\(value) is divisible by 3 & 5 \(resultArray)")
+            } else if value % 3 == 0 {
+                resultArray.append(fizz)
+                print(resultArray)
+                print("\(value) is divisible by 3: \(resultArray)")
             } else {
                 resultArray.append(value)
+                print(resultArray)
                 print("\(value) is not divisible by 3 or 5: \(resultArray)")
             }
         }
-        return true
     }
 }
 
